ec87b44b532dc8113efbc87dcd4f97a7db75771c
Conor O shea <47694309+conoroshea1996@users.noreply.github.com>
2019-04-25 16:10:01 +0100

    Confirm Ending
    Confirm the ending of a string

---
+++ ConfirmEnding/ConfirmEnding.js
@@ -0,0 +1,22 @@
+// Check out endsWith Mehod here https://www.w3schools.com/jsref/jsref_endswith.asp ///
+function ConfirmEnd(string,end) {
+    if(string.endsWith(end)){// End method which checks if a string ends with 
+        return true;
+    }else{
+        return false;
+    };
+}
+console.log(ConfirmEnd('conor','r'));
+
+//------------- Using Substr ---------------------------//
+// substr https://www.w3schools.com/jsref/jsref_substr.asp ///
+
+function ConfirmEnd1(string,end) {
+  let target = string.substr(-end.length);// returns a string the lenght of end
+   if( target === end){ // check to see if string is eqaul to end 
+       return true;
+   } else{
+       return false;
+   }
+}
+console.log(ConfirmEnd1('Your a wizard Harry',' Harry'));
