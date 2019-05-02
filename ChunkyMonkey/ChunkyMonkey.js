91a4d18311e70b4b90d690a174e075acda3e2279
Conor O shea <47694309+conoroshea1996@users.noreply.github.com>
2019-05-02 21:31:00 +0100

    Update ChunkyMonkey.js

--- ChunkyMonkey/ChunkyMonkey.js
+++ ChunkyMonkey/ChunkyMonkey.js
@@ -1,6 +1,6 @@
 function chunkArrayInGroups(array,num) {
         let finalArray =[];// Setting a varible to an empty array
-        i=0; // setting variable to value of 0
+        let i=0; // setting variable to value of 0
         while( i< array.length){ //while i is less than the length of array do this
          finalArray.push(array.slice(i,i+=num)); // put a slice from i to i +num of our array into finalArray 
         }
